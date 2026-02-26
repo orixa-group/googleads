af007a8595933bb37d7c6fe601f603374b567793
Romain Jeannoutot <romain.jeannoutot@gmail.com>
2026-02-26 15:24:50 +0100

    feat: implement customer creation

--- customer.go
+++ customer.go
@@ -14,11 +14,27 @@ type Customer struct {
 }
 
 func NewCustomer() *Customer {
-	return &Customer{&resources.Customer{}}
+	return &Customer{&resources.Customer{
+		CurrencyCode: String("EUR"),
+		TimeZone:     String("Europe/Paris"),
+	}}
 }
 
 func (c Customer) GetId() string {
 	return strconv.Itoa(int(c.Customer.GetId()))
+}
+
+func (c *Customer) SetId(id string) {
+	i, _ := strconv.ParseInt(id, 10, 64)
+	c.Customer.Id = Int64(i)
+}
+
+func (c Customer) GetName() string {
+	return c.Customer.GetDescriptiveName()
+}
+
+func (c *Customer) SetName(name string) {
+	c.Customer.DescriptiveName = String(name)
 }
 
 func (c Customer) ListCampaigns(ctx context.Context) (Campaigns, error) {
@@ -27,6 +43,16 @@ func (c Customer) ListCampaigns(ctx context.Context) (Campaigns, error) {
 
 func (c Customer) FetchCampaign(ctx context.Context, id string) (*Campaign, error) {
 	return FetchCampaign(ctx, c.GetId(), CampaignById(id))
+}
+
+func (c *Customer) Create(ctx context.Context, parent *Customer) error {
+	customer, err := CreateCustomer(ctx, c, parent)
+	if err != nil {
+		return err
+	}
+
+	c.Customer = customer.Customer
+	return nil
 }
 
 func (c *Customer) CreateBillingSetup(ctx context.Context, paymentsAccountId string) (*BillingSetup, error) {
--- customer.service.go
+++ customer.service.go
@@ -3,6 +3,7 @@ package googleads
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/shenzhencenter/google-ads-pb/services"
 )
@@ -13,4 +14,16 @@ func FetchCustomer(ctx context.Context, id string) (*Customer, error) {
 	}).Build(), func(row *services.GoogleAdsRow) *Customer {
 		return &Customer{row.GetCustomer()}
 	})
+}
+
+func CreateCustomer(ctx context.Context, customer, parent *Customer) (*Customer, error) {
+	resp, err := services.NewCustomerServiceClient(instance.conn).CreateCustomerClient(ctx, &services.CreateCustomerClientRequest{
+		CustomerId:     parent.GetId(),
+		CustomerClient: customer.Customer,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return FetchCustomer(ctx, strings.Split(resp.GetResourceName(), "/")[1])
 }
