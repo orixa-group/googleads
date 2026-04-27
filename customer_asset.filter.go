c1567aea6fce54e3d42eaecb88a840692c9256dd
Romain Jeannoutot <romain.jeannoutot@gmail.com>
2026-04-27 16:46:15 +0200

    fix: filter customer asset by customer resource name

--- customer_asset.filter.go
+++ customer_asset.filter.go
@@ -18,6 +18,6 @@ func CustomerAssetByAsset(resourceName string) CustomerAssetFilter {
 
 func CustomerAssetByCustomer(resourceName string) CustomerAssetFilter {
 	return func() string {
-		return fmt.Sprintf("customer_asset.customer = '%s'", resourceName)
+		return fmt.Sprintf("customer.resource_name = '%s'", resourceName)
 	}
 }
