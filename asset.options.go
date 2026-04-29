8318bc9b27479e6e3aa99bfd0f071fddae59f5ae
Romain Jeannoutot <romain.jeannoutot@gmail.com>
2026-04-29 16:04:47 +0200

    feat: keep site link asset link text on change asset url

--- asset.options.go
+++ asset.options.go
@@ -21,9 +21,8 @@ func ChangeAssetPhoneNumber(number string) AssetOption {
 
 func ChangeAssetURL(url string) AssetOption {
 	return func(a *resources.Asset) {
-		if data, ok := a.GetAssetData().(*resources.Asset_SitelinkAsset); ok {
+		if _, ok := a.GetAssetData().(*resources.Asset_SitelinkAsset); ok {
 			a.FinalUrls = []string{url}
-			data.SitelinkAsset.LinkText = url
 		}
 	}
 }
